d7863eb2b2104e22f7cb40cee2e74e950fea366a
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2021-04-28 15:53:48 -0400

    Inititial commit maps.cpp

---
+++ maps.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+// https://www.hackerrank.com/challenges/linkedin-practice-dictionaries-and-maps/problem
+
+
+int main() {
+
+    string paragraph = "A planet is an astronomical body orbiting a star or stellar remnant that is massive enough to be rounded by its own gravity";
+
+    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+
+    }    
+
+}
